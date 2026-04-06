792f4c48294aff1738c4ce6c6a804894a8f0ae36
IvanLH-GC <han.lee@student.griffith.ie>
2026-04-06 15:46:39 +0100

    Added a post logic

--- app/server/src/routes/userRoutes.js
+++ app/server/src/routes/userRoutes.js
@@ -2,3 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
+// Post a new user
+router.post('/', async (req, res) => {
+  const user = new User(req.body);
+  await user.save();
+  res.json(user);
+});
+
